788abe6ec4c02710e8413bee584847fc05615d36
cindy <cimpylula14@hotmail.de>
2023-08-09 10:16:01 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# beauty_geek_cr
+# Portafolio del proyecto
+
+- link del proyecto: <a href="https://brayantorresa.github.io/AluraGeek-Ecommerce/" target="_blank">Ir a la tienda</a>
+# portfoliocr
---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,223 @@
+
+export const lista = [{
+  titulo: "Maquillaje",
+  produtos: [{
+    infos: {
+      id: 1,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card.jpg",
+      precio: 30.90,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card2.jpg",
+      precio: 40.90,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card3.jpg",
+      precio: 50.90,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card4.jpg",
+      precio: 60.89,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card5.jpg",
+      precio: 60.78,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card6.jpg",
+      precio: 39.99,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card7.jpg",
+      precio: 39.99,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Productos"
+    }
+  }
+  ]
+},
+{
+  titulo: "Cuidados de la piel",
+  produtos: [{
+    infos: {
+      id: 1,
+      nombre: "Maquillaje",
+      img: "../../../assets/img/card8.jpg",
+      precio: 29.00,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Maquillaje"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nombre: "Crema",
+      img: "../../../assets/img/card9.jpg",
+      precio: 19.89,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Cremas"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nombre: "Perfume",
+      img: "../../../assets/img/card10.jpg",
+      precio: 29.89,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Cremas"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nombre: "MAquillaje",
+      img: "../../../assets/img/card11.jpg",
+      precio: 49.89,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Cremas"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nombre: "Maquillaje",
+      img: "../../../assets/img/card12.jpg",
+      precio: 40.00,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Maquillaje"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nombre: "Perfume",
+      img: "../../../assets/img/card13.jpg",
+      precio: 30.50,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Brochas"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nombre: "Brochas",
+      img: "../../../assets/img/card14.jpg",
+      precio: 60.49,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Brochas"
+    }
+  }
+  ]
+},
+{
+  titulo: "Diversos",
+  produtos: [{
+    infos: {
+      id: 1,
+      nombre: "Camisa Atari",
+      img: "../../../assets/img/card15.jpg",
+      precio: 50.90,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nombre: "Brochas",
+      img: "../../../assets/img/card16.jpg",
+      precio: 40.89,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nombre: "Crema",
+      img: "../../../assets/img/card17.jpg",
+      precio: 29.90,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nombre: "Perfume",
+      img: "../../../assets/img/card18.jpg",
+      precio: 60.01,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nombre: "Sombras de ojos",
+      img: "../../../assets/img/card19.jpg",
+      precio: 60.01,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nombre: "Producto XYZ",
+      img: "../../../assets/img/card20.jpg",
+      precio: 60.01,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nombre: "Maquillaje",
+      img: "../../../assets/img/card21.jpg",
+      precio: 60.01,
+      descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      alt: "Diversos"
+    }
+  }
+  ]
+}]
